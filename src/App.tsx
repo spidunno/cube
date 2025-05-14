b85cb1686a79c8fdbf6f6bc4f5a510ee6b12d6ef
spidunno <unnospid@gmail.com>
2025-05-14 09:12:32 -0400

    install joy ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,19 @@
+import { Link, Route, Switch } from "wouter";
+import Home from "./pages";
+
 export default function App() {
-  return <>test</>;
+	return (
+		<>
+			<Switch>
+				<Route path="/" component={Home} />
+
+				<Route path="/app">
+					{(params) => <>Hello, test!</>}
+				</Route>
+
+				{/* Default route in a switch */}
+				<Route>404: No such page!</Route>
+			</Switch>
+		</>
+	);
 }
